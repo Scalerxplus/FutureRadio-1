30f58f98a4971e55a661f410cd0709d19d98570e
Scalerxplus <phoenixmultimediain@gmail.com>
2026-05-30 20:26:04 +0530

    Revert CSS logo attempt and restore generated images

--- components/ui/CinematicSplash.tsx
+++ components/ui/CinematicSplash.tsx
@@ -66,9 +66,9 @@ export default function CinematicSplash({ onComplete }: { onComplete: () => void
             />
             
             <img 
-              src="/logo-horizontal.png" 
+              src="/logo-dark-theme.png" 
               alt="Future Radio" 
-              className="w-auto h-[210px] md:h-[260px] object-contain drop-shadow-[0_0_20px_rgba(255,255,255,0.2)] filter invert hue-rotate-180" 
+              className="w-auto h-[210px] md:h-[260px] object-contain drop-shadow-[0_0_20px_rgba(255,255,255,0.2)]" 
             />
           </motion.div>
         </motion.div>
